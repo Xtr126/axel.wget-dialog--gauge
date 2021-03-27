dcef8aa12ce52257099f86f37d8f876b0e4f893b
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2021-03-27 22:29:43 +0530

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -6,7 +6,7 @@ while read -r data
 do
 cat <<EOF
 XXX
-$(echo "$data" | stdbuf -o0 awk '{ print $7 }' | tr -d '%')
+$(echo "$data" | stdbuf -o0 awk '{ print $7+0 }')
 wget stdout raw -> $(echo "$data")
 Download speed -> $(echo "$data" | stdbuf -o0 awk '{ print $8 }') Downloaded data -> $(echo "$data" | stdbuf -o0 awk '{ print $1 }')
 Estimated time for end -> $(echo "$data" | stdbuf -o0 awk '{ print $9 }') 
