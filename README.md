caab36a6a2cb68db8535c47d02bf1ca0956965e1
Xtr126 <crawleymsh@gmail.com>
2022-10-04 09:38:39 +0530

    wget.sh: rewrite in pure bash

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # axel.wget-dialog--gauge
 Simple bash scripts to process axel wget rsync output and send to dialog gui, including time left, speed and all output from the commands. 
-It might have high CPU usage.
--- wget.sh
+++ wget.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-Loader() {
-(
-while read -r data
-do
-cat <<EOF
-XXX
-$(echo "$data" | stdbuf -o0 awk '{ print $7+0 }')
-wget stdout raw -> $(echo "$data")
-Download speed -> $(echo "$data" | stdbuf -o0 awk '{ print $8 }') Downloaded data -> $(echo "$data" | stdbuf -o0 awk '{ print $1 }')
-Estimated time for end -> $(echo "$data" | stdbuf -o0 awk '{ print $9 }') 
-XXX
-EOF
-done
-) |
-dialog --title "$URL" "$@" --gauge "Xtr" 10 55
+
+if ! [ -x "$(command -v dialog)" ]; then echo "dialog needs to be installed first"; exit 1; fi
+
+# 0 - 100K 
+# 6 - 67%
+# 7 - 10.4K (speed)
+# 8 - 2m28s (time)  
+
+function process() {
+    while read -a data; do
+        echo XXX
+        echo ${data[6]//[!0-9]/}
+        echo "Download speed -> ${data[7]}"  
+        echo "Downloaded data -> ${data[0]}"
+        echo "Time left -> ${data[8]}"
+        echo XXX
+    done
 }
-wget "$@" 2>&1 | Loader
+
+wget "$@" 2>&1 | process | dialog --title "wget $*" --gauge "Initializing.." 8 40
 
